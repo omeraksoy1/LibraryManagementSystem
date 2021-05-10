8cf32e38de903072861c10852ffd8c89322bad18
Can Koz <canxkoz@gmail.com>
2021-05-10 13:34:28 +0300

    Insert / Search / Delete Books
    Contains files related to searching deleting and inserting books

---
+++ delete_book.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+<body>
+
+    <form action='result.php' method='post'>
+        <label>bookID:</label><input type='number' name='bookID' /><input name="delete_book", value='delete' type='submit'/></p>
+    </form>
+    <hr>
+
+	<br>
+
+	<form action="book.php">
+		<input type="submit" value="Back to Book Operations" />
+	</form>
+
+</body>
+</html>
---
+++ insert_book.php
@@ -0,0 +1,22 @@
+<html>
+<head></head>
+<body>
+
+    <form action='result.php' method='post'>
+        <label>bookID:         </label> <input type='number' name='bookID' /><br/>
+        <label>title:        </label><input type='text' name='title' /><br/>
+        <label>authors: </label><input type='text' name='authors' /><br/>
+        <label>average_rating:    </label><input type='number' name='average_rating' /><br/>
+        <label>isbn:  </label><input type='number' name='isbn' /><br/>
+        <input name="insert_book", value='Insert' type='submit'/></p>
+    </form>
+    <hr>
+
+	<br>
+
+	<form action="book.php">
+		<input type="submit" value="Back to Book Operations" />
+	</form>
+
+</body>
+</html>
